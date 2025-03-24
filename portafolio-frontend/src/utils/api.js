6bdd2ae64d7a9d1e2090d54f2993f640c548a1f7
Alexis Gallardo <ing.bits@outlook.es>
2025-03-24 20:20:41 -0300

    Actualiza utils.js para desplegar

--- portafolio-frontend/src/utils/api.js
+++ portafolio-frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000', // Sin /api/ al final
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://agmzdev25.pythonanywhere.com',
     headers: {
         'Content-Type': 'application/json'
     },
